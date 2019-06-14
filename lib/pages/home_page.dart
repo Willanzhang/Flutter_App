a9fa0058d1adc6f678dc65d3a7743cf28137c404
unknown <631871612@qq.com>
2019-06-14 08:11:44 +0800

    add goods recommend

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -41,14 +41,19 @@ class _HomePageState extends State<HomePage> {
 						String adPicture = data['data']['advertesPicture']['PICTURE_ADDRESS'].toString();
 						String  leaderImage = data['data']['shopInfo']['leaderImage'];
 						String  leaderPhone = data['data']['shopInfo']['leaderPhone'];
+						List<Map> recommendList = (data['data']['recommend'] as List).cast();
+						recommendList += recommendList;
 						// List swipter = [{'image': 'http://images.baixingliangfan.cn/advertesPicture/20190116/20190116173351_2085.jpg', 'goodsId': '6fe4fe0fb5394c0d9b9b4792a827e029'},{'image': 'http://images.baixingliangfan.cn/advertesPicture/20190116/20190116173351_2085.jpg', 'goodsId': '6fe4fe0fb5394c0d9b9b4792a827e029'}];
-						return Column(
-							children: <Widget>[
-								SwiperDiy(swiperDateList:swipter),
-								TopNavigator(navigatorList: navigatorList),
-								AdBanner(advertesPicture: adPicture),
-								LeaderPhone(leaderImage: leaderImage, leaderPhone: leaderPhone,)
-							],
+						return SingleChildScrollView(
+						  child: Column(
+						  	children: <Widget>[
+						  		SwiperDiy(swiperDateList:swipter),
+						  		TopNavigator(navigatorList: navigatorList),
+						  		AdBanner(advertesPicture: adPicture),
+						  		LeaderPhone(leaderImage: leaderImage, leaderPhone: leaderPhone),
+									Recommend(recommendList: recommendList,)
+						  	],
+						  ),
 						);
 					} else {
 						return new Center(
@@ -170,5 +175,90 @@ class LeaderPhone extends StatelessWidget {
 		} else {
 			throw 'Could not launch $url';
 		}
+	}
+}
+
+// 商品推介
+class Recommend extends StatelessWidget {
+	final List recommendList;
+	Recommend({Key key, this.recommendList}) : super(key: key);
+	
+
+	// 标题方法
+	Widget _titleWidget() {
+		return Container(
+			alignment: Alignment.centerLeft,
+			padding: EdgeInsets.fromLTRB(10.0, 2.0, 0, 5.0),
+			decoration: BoxDecoration(
+				color: Colors.white,
+				border: Border(
+					bottom: BorderSide(width: 0.5, color: Colors.black12)
+				)
+			),
+			child: Text(
+				'商品推介',
+				style: TextStyle(color: Colors.pink)
+			),
+		);
+	}
+
+	// 商品单独项
+	Widget _item(index) {
+		return InkWell(
+			child: Container(
+				height: ScreenUtil().setHeight(330),
+				width: ScreenUtil().setWidth(250),
+				padding: EdgeInsets.all(8.0),
+				decoration: BoxDecoration(
+					color: Colors.white,
+					border: Border(
+						left: BorderSide(width: 0.5, color: Colors.black12)
+					)
+				),
+				child: Column(
+					children: <Widget>[
+						Image.network(recommendList[index]['image']),
+						Text('￥${recommendList[index]['mallPrice']}'),
+						Text(
+							'￥${recommendList[index]['price']}',
+							style: TextStyle(
+								// 下划线
+								decoration: TextDecoration.lineThrough,
+								color: Colors.grey
+							),
+							),
+					],
+				),
+			),
+		);
+	}
+
+	// 横向列表
+	Widget _recommentList() {
+		return Container(
+			height: ScreenUtil().setHeight(330),
+			margin: EdgeInsets.only(top: 10.0),
+			child: ListView.builder(
+				scrollDirection: Axis.horizontal,
+				itemCount: recommendList.length,
+				itemBuilder: (context, index) {
+					return _item(index);
+				},
+			)
+		);
+	}
+
+	@override
+	Widget build(BuildContext context) {
+		return Container(
+			height: ScreenUtil().setHeight(380),
+			margin: EdgeInsets.only(top: 10.0),
+			child: Column(
+				children: <Widget>[
+					_titleWidget(),
+					_recommentList()
+				],
+			),
+		);
 	}
 }
