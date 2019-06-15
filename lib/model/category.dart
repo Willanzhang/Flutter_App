b163ad738c0fba1ff2fe2dd978466c52bd2362f0
unknown <631871612@qq.com>
2019-06-15 11:56:53 +0800

    左侧导航

--- lib/model/category.dart
+++ lib/model/category.dart
@@ -1,38 +1,134 @@
+// class CategoryModel {
+// 	String mallCategoryId; // 类别编号
+// 	String mallCategoryName; // 类别名称
+// 	List<dynamic> bxMallSubDto;
+// 	Null comments;
+// 	String image;
+
+// 	CategoryModel({
+// 		this.mallCategoryId,
+// 		this.mallCategoryName,
+// 		this.bxMallSubDto,
+// 		this.comments,
+// 		this.image,
+// 	});
+
+// 	// 工厂构造模式 不用使用new 也是给 类添加一个fromJson的静态方法而已 fromJson是自己定义的
+// 	factory CategoryModel.fromJson(dynamic json){
+// 		return CategoryModel(
+// 			mallCategoryId: json['mallCategoryId'],
+// 			mallCategoryName: json['mallCategoryName'],
+// 			bxMallSubDto: json['bxMallSubDto'],
+// 			comments: json['comments'],
+// 			image: json['image'],
+// 		);
+// 	}
+// }
+
+// class CategoryListModel {
+// 	List<CategoryModel> data;
+// 	CategoryListModel(this.data);
+
+// 	factory CategoryListModel.fromJson(List json) {
+// 		return CategoryListModel(
+// 			// CategoryModel.fromJson((i))  这么写的原因是 CategoryModel？
+// 			json.map((i)=>CategoryModel.fromJson(i)).toList()
+// 		);
+// 	}
+// }
+
+
 class CategoryModel {
-	String mallCategoryId; // 类别编号
-	String mallCategoryName; // 类别名称
-	List<dynamic> bxMallSubDto;
-	Null comments;
-	String image;
+  String code;
+  String message;
+  List<CategoryData> data;
 
-	CategoryModel({
-		this.mallCategoryId,
-		this.mallCategoryName,
-		this.bxMallSubDto,
-		this.comments,
-		this.image,
-	});
+  CategoryModel({this.code, this.message, this.data});
 
-	// 工厂构造模式 不用使用new 也是给 类添加一个fromJson的静态方法而已 fromJson是自己定义的
-	factory CategoryModel.fromJson(dynamic json){
-		return CategoryModel(
-			mallCategoryId: json['mallCategoryId'],
-			mallCategoryName: json['mallCategoryName'],
-			bxMallSubDto: json['bxMallSubDto'],
-			comments: json['comments'],
-			image: json['image'],
-		);
-	}
+  CategoryModel.fromJson(Map<String, dynamic> json) {
+    code = json['code'];
+    message = json['message'];
+    if (json['data'] != null) {
+      data = new List<CategoryData>();
+      json['data'].forEach((v) {
+        data.add(new CategoryData.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['code'] = this.code;
+    data['message'] = this.message;
+    if (this.data != null) {
+      data['data'] = this.data.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
 }
 
-class CategoryListModel {
-	List<CategoryModel> data;
-	CategoryListModel(this.data);
+class CategoryData {
+  String mallCategoryId;
+  String mallCategoryName;
+  List<BxMallSubDto> bxMallSubDto;
+  Null comments;
+  String image;
 
-	factory CategoryListModel.fromJson(List json) {
-		return CategoryListModel(
-			// CategoryModel.fromJson((i))  这么写的原因是 CategoryModel？
-			json.map((i)=>CategoryModel.fromJson(i)).toList()
-		);
-	}
+  CategoryData(
+      {this.mallCategoryId,
+      this.mallCategoryName,
+      this.bxMallSubDto,
+      this.comments,
+      this.image});
+
+  CategoryData.fromJson(Map<String, dynamic> json) {
+    mallCategoryId = json['mallCategoryId'];
+    mallCategoryName = json['mallCategoryName'];
+    if (json['bxMallSubDto'] != null) {
+      bxMallSubDto = new List<BxMallSubDto>();
+      json['bxMallSubDto'].forEach((v) {
+        bxMallSubDto.add(new BxMallSubDto.fromJson(v));
+      });
+    }
+    comments = json['comments'];
+    image = json['image'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['mallCategoryId'] = this.mallCategoryId;
+    data['mallCategoryName'] = this.mallCategoryName;
+    if (this.bxMallSubDto != null) {
+      data['bxMallSubDto'] = this.bxMallSubDto.map((v) => v.toJson()).toList();
+    }
+    data['comments'] = this.comments;
+    data['image'] = this.image;
+    return data;
+  }
+}
+
+class BxMallSubDto {
+  String mallSubId;
+  String mallCategoryId;
+  String mallSubName;
+  String comments;
+
+  BxMallSubDto(
+      {this.mallSubId, this.mallCategoryId, this.mallSubName, this.comments});
+
+  BxMallSubDto.fromJson(Map<String, dynamic> json) {
+    mallSubId = json['mallSubId'];
+    mallCategoryId = json['mallCategoryId'];
+    mallSubName = json['mallSubName'];
+    comments = json['comments'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['mallSubId'] = this.mallSubId;
+    data['mallCategoryId'] = this.mallCategoryId;
+    data['mallSubName'] = this.mallSubName;
+    data['comments'] = this.comments;
+    return data;
+  }
 }
--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../service/service_method.dart';
 // 装换data数据格式
 import 'dart:convert';
@@ -13,20 +14,83 @@ class CategoryPage extends StatefulWidget {
 class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
-    _getCategory();
-    return Container(
-      child: Center(
-        child: Text('分类页面'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('商品分类'),
+      ),
+      body: Container(
+        child: Row(
+          children: <Widget>[
+            LeftCategoryNav()
+          ],
+        ),
       ),
     );
   }
+
+}
+
+// 左侧大类导航
+class LeftCategoryNav extends StatefulWidget {
+  @override
+  _LeftCategoryNavState createState() => _LeftCategoryNavState();
+}
+
+class _LeftCategoryNavState extends State<LeftCategoryNav> {
+  List list = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _getCategory();
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: ScreenUtil().setWidth(180),
+      decoration: BoxDecoration(
+        border: Border(
+          right: BorderSide(color: Colors.black12, width: 1)
+        )
+      ),
+      child: ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (BuildContext context, index) {
+          return _leftInkWell(index);
+        },
+      ),
+    );
+  }
+
+  Widget _leftInkWell(int index) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        height: ScreenUtil().setHeight(100),
+        padding: EdgeInsets.only(left: 10, top: 20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom: BorderSide(width: 1, color: Colors.black12)
+          )
+        ),
+        child: Text(list[index].mallCategoryName, style: TextStyle(fontSize: ScreenUtil().setSp(28)),),
+      ),
+    );
+  }
+
 
   void _getCategory() async{
     print('=======> 这里是第二个页面');
     await request('getCategory').then((val) {
       var data = json.decode(val.toString());
-      CategoryListModel list = CategoryListModel.fromJson(data['data']);
-      list.data.forEach((item) => print(item.mallCategoryName));
+      CategoryModel category = CategoryModel.fromJson(data);
+      // print('-------------->${xx}');
+      setState(() {
+        list = category.data;
+      });
+      category.data.forEach((item) => print(item.mallCategoryName));
     });
   }
 }
