6689471ee3e6ef7b6b668a8c61ff649a0712ad6b
unknown <631871612@qq.com>
2019-06-16 23:21:20 +0800

    持久化 出现安装问题

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,59 +1,90 @@
 import 'package:flutter/material.dart';
-import 'package:provide/provide.dart';
-import '../provide/counter.dart';
-import '../provide/age.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class CartPage extends StatelessWidget {
-	@override
-	Widget build(BuildContext context) {
-		return Scaffold(
-				body: Center(
-					child: Column(
-						children: <Widget>[
-							Number(),
-							MyButton(),
-						],
-					),
-		));
-	}
+class CartPage extends StatefulWidget {
+  @override
+  _CartPageState createState() => _CartPageState();
 }
 
-class Number extends StatelessWidget {
-	@override
-	Widget build(BuildContext context) {
-		return Container(
-			margin: EdgeInsets.only(top: 200),
-			child: Provide<Counter>(
-				builder: (BuildContext context, child, counter) {
-					return Column(
-						children: <Widget>[
-							Text('${counter.value}', style: Theme.of(context).textTheme.display1,),
-							Provide<Age>(
-                builder: (context, child, age) {
-                  print(age.age);
-                  print('!!');
-                  return Text('${age.age}', style: Theme.of(context).textTheme.display1,);
-                },
-              )
-						],
-					);
-				},
-			)
-		);
-	}
-}
+class _CartPageState extends State<CartPage> {
 
-class MyButton extends StatelessWidget {
-	@override
-	Widget build(BuildContext context) {
-		return Container(
-			child: RaisedButton(
-				onPressed: () {
-					Provide.value<Counter>(context).increment();
-					print('button');
-				}, 
-				child: Text('递增'),
-			),
-		);
-	}
+  List<String> testList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    // 進入的時候是要查詢的
+    _show();
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 500,
+            child: ListView.builder(
+              itemCount: testList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(testList[index]),
+                );
+              },
+            ),
+          ),
+          RaisedButton(
+            onPressed: () {
+              _add();
+            },
+            child: Text('增加'),
+          ),
+          RaisedButton(
+            onPressed: () {
+              _remove();
+            },
+            child: Text('清空'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // 增加方法  必須使用異步方法
+  void _add() async {
+    // 初始化時需要時間的
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String temp = '要做最靚的'; // 每點一次加一個臨時的變量
+    testList.add(temp);
+    // 設置set
+    prefs.setStringList('testInfo', testList);
+    _show();
+  }
+
+  // 查詢
+  void _show() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    // 獲取 get
+    if (prefs.getStringList('testInfo') != null) {
+      setState(() {
+        testList = prefs.getStringList('testInfo');
+      });
+    }
+  }
+
+  // 刪除
+  void _remove() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    // prefs.clear() 清空所有 的值，  包括 key=testInfo
+    prefs.remove('testInfo');
+    setState(() {
+      testList = [];
+    });
+  }
+
+  // 清空
+  // void _clear() async {
+  //   SharedPreferences prefs = await SharedPreferences.getInstance();
+  //   //  清空所有
+  //   prefs.clear();
+  //   setState(() {
+  //     testList = [];
+  //   });
+  //   // prefs.remove('testInfo');
+  // }
 }
--- lib/pages/member_page.dart
+++ lib/pages/member_page.dart
@@ -4,56 +4,55 @@ import '../provide/counter.dart';
 import '../provide/age.dart';
 
 class MemberPage extends StatelessWidget {
-  TextEditingController ageCount = TextEditingController();
-  int age = 0;
-  @override
-  void initState () {
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget>[
-              Provide<Counter>(
-                builder: (context, child, counter){
-                  return Text('${counter.value}', style: Theme.of(context).textTheme.display1,);
-                },
-              ),
-              TextField(
-                controller: ageCount,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(10.0),
-                  labelText: '年龄',
-                  helperText: '请输入年龄'
-                ),
-                autofocus: false,
-              ),
-              RaisedButton(
-                onPressed: () {
-                  age = int.parse(ageCount.text);
-                  print('---------> $age');
-                  Provide.value<Age>(context).setAge(age);
-                },
-                child: Text('点击改变年龄'),
-              ),
-              Text('${Provide.value<Age>(context).age}'),
-              Provide<Age>(
-                builder: (context, child, age) {
-                  print(age.age);
-                  print('!!');
-                  return Text('${age.name}', style: Theme.of(context).textTheme.display1,);
-                },
-              )
+	
+	TextEditingController ageCount = TextEditingController();
+	int age = 0;
 
-              // Provide<Counter>(
-              //   builder: (context, child, counter){
-              //     return Text('${counter.value}', style: Theme.of(context).textTheme.display1,);
-              //   },
-              // ),
-          ],
-        )
-      )
-    );
-  }
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			body: Center(
+				child: Column(
+					children: <Widget>[
+							Provide<Counter>(
+								builder: (context, child, counter){
+									return Text('${counter.value}', style: Theme.of(context).textTheme.display1,);
+								},
+							),
+							TextField(
+								controller: ageCount,
+								decoration: InputDecoration(
+									contentPadding: EdgeInsets.all(10.0),
+									labelText: '年龄',
+									helperText: '请输入年龄'
+								),
+								autofocus: false,
+							),
+							RaisedButton(
+								onPressed: () {
+									age = int.parse(ageCount.text);
+									print('---------> $age');
+									Provide.value<Age>(context).setAge(age);
+								},
+								child: Text('点击改变年龄'),
+							),
+							Text('${Provide.value<Age>(context).age}'),
+							Provide<Age>(
+								builder: (context, child, age) {
+									print(age.age);
+									print('!!');
+									return Text('${age.name}', style: Theme.of(context).textTheme.display1,);
+								},
+							)
+
+							// Provide<Counter>(
+							//   builder: (context, child, counter){
+							//     return Text('${counter.value}', style: Theme.of(context).textTheme.display1,);
+							//   },
+							// ),
+					],
+				)
+			)
+		);
+	}
 }
