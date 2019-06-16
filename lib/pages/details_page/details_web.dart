9a01af364dc2559e370ec6af0da22dd54074fa75
unknown <631871612@qq.com>
2019-06-16 22:02:49 +0800

    商品详情详情以及评论切换

--- lib/pages/details_page/details_web.dart
+++ lib/pages/details_page/details_web.dart
@@ -10,11 +10,27 @@ class DetailsWeb extends StatelessWidget {
 
 		var goodsDetail = Provide.value<DetailInfoProvide>(context).goodsInfo.data.goodInfo.goodsDetail;
 		print('---------------------->$goodsDetail');
-		return Container(
-			child: Html(
-				// 不支持 100%這種單位  作者說在以後更改
-				data: goodsDetail
-			),
-		);
+		return Provide<DetailInfoProvide>(
+				builder: (context, child, val) {
+					var isLeft = Provide.value<DetailInfoProvide>(context).isLeft;
+					if (isLeft) {
+						return Container(
+											child: Html(
+												// 不支持 100%這種單位  作者說在以後更改
+												data: goodsDetail
+											),
+										);
+					} else {
+						return Container(
+							width: ScreenUtil().setWidth(750),
+							padding: EdgeInsets.all(10.0),
+							// 文字居中
+							alignment: Alignment.center,
+							child: Text('暫時沒有數據'),
+						);
+					}
+				},
+			);
+		
 	}
 }
